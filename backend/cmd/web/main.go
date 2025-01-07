f9711a959d5ddd84a6196460d8a9e6a0aa22a092
Jorge Bortolotti Suarez <jbortolo@car15s2.42barcelona.com>
2025-01-07 15:41:23 +0100

    subiendo cosas

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"context"
+	"os"
 	
 	"github.com/twpayne/go-geos"
     pgxgeos "github.com/twpayne/pgx-geos"
@@ -13,19 +14,20 @@ import (
 
 func main() {
 	serv := http.NewServeMux()
+	ctx := context.Background()
 
 	databaseStr := os.Getenv("DB_URL")
     conn, err := pgx.Connect(context.Background(), databaseStr)
     if err != nil {
-        return err
+        log.Fatal(err.Error())
     }
     if err := pgxgeos.Register(ctx, conn, geos.NewContext()); err != nil {
-        return err
+        log.Fatal(err.Error())
     }
 
-	config, err := pgxpool.ParseConfig(connectionStr)
+	config, err := pgxpool.ParseConfig(databaseStr)
     if err != nil {
-        return err
+        log.Fatal(err.Error())
     }
     config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
         if err := pgxgeos.Register(ctx, conn, geos.NewContext()); err != nil {
@@ -36,7 +38,7 @@ func main() {
 
     pool, err := pgxpool.NewWithConfig(context.Background(), config)
     if err != nil {
-        return err
+        log.Fatal(err.Error())
     }
 	defer pool.Close()
 	
@@ -48,5 +50,5 @@ func main() {
 
 	log.Println("Starting server on :3000")
 	err = http.ListenAndServe(":3000", serv)
-	log.Fatal(err)
+	log.Fatal(err.Error())
 }
